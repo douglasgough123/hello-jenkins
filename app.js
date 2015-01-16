0079789fda6c4333d6ccc6eec11776bd5907e1b4
Doug Gough <douglas.gough@1800gotjunk.com>
2015-01-16 10:48:29 -0800

    Making changes. Testing Jenkins

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Dave. I like your special hat.');
+  res.send('Hello Dave. I like your ugly hat.');
 });
 
 app.listen(process.env.PORT || 5000);

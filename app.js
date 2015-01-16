36340ae4edc98888bc52946ea15f6635f881f317
Doug Gough <douglas.gough@1800gotjunk.com>
2015-01-16 11:37:16 -0800

    Fix error in test script

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Dave. I like your ugly hat.');
+  res.send('Hello Dave. I like your ugly stupid hat.');
 });
 
 app.listen(process.env.PORT || 5000);
